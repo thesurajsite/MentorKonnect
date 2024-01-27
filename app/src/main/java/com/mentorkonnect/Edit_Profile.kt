47b22865394abdb2e5639cde2ee53714e96bf7cb
Suraj <thesurajsite@gmail.com>
2024-01-27 22:49:24 +0530

    add user data to firestore successful

--- app/src/main/java/com/mentorkonnect/Edit_Profile.kt
+++ app/src/main/java/com/mentorkonnect/Edit_Profile.kt
@@ -1,20 +1,69 @@
 package com.mentorkonnect
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.jar.Attributes
 
 class Edit_Profile : AppCompatActivity() {
+
+    private val db: FirebaseFirestore by lazy {Firebase.firestore}
+
+    private val USER= MainActivity.auth.currentUser?.uid.toString()
+    private val USER_INFO="UserInfo"
+    private val NAME="name"
+    private val BIO="bio"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
+
+
         val userName=findViewById<EditText>(R.id.user_Name)
         val userBio=findViewById<EditText>(R.id.userBio)
         val saveChanges=findViewById<Button>(R.id.saveChanges)
+        val progressBar=findViewById<ProgressBar>(R.id.Progressbar)
+
+
+        saveChanges.setOnClickListener {
+
+            progressBar.visibility=ProgressBar.VISIBLE
+
+//            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+            val name=userName.text.toString()
+            val bio=userBio.text.toString()
+
+            val map= mutableMapOf<String, String>()
+            map.put(NAME, name)
+            map.put(BIO, bio)
+            db.collection(USER_INFO).document(USER).set(map)
+                .addOnSuccessListener {
+                    progressBar.visibility=ProgressBar.INVISIBLE
+                    Toast.makeText(this, "Changes Saved Successfully", Toast.LENGTH_SHORT).show()
+
+
+                }
+                .addOnFailureListener {
+                    progressBar.visibility=ProgressBar.INVISIBLE
+                    Toast.makeText(this, "Error: $it", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+
+
+
+
 
 
 
     }
+
 }
--- app/src/main/java/com/mentorkonnect/LoginActivity.kt
+++ app/src/main/java/com/mentorkonnect/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.text.method.PasswordTransformationMethod
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
@@ -19,6 +20,7 @@ class LoginActivity : AppCompatActivity() {
         val checkbox=findViewById<CheckBox>(R.id.checkbox)
         val login_btn=findViewById<Button>(R.id.login_btn)
         val signup_btn=findViewById<Button>(R.id.login_to_signup_btn)
+        val progressBar=findViewById<ProgressBar>(R.id.Progressbar)
 
         checkbox.setOnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked){
@@ -31,11 +33,14 @@ class LoginActivity : AppCompatActivity() {
         }
 
         signup_btn.setOnClickListener {
+            progressBar.visibility=ProgressBar.VISIBLE
             val intent=Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+            finish()
         }
 
         login_btn.setOnClickListener {
+            progressBar.visibility=ProgressBar.VISIBLE
             val email=email_et.text.toString()
             val password=password_et.text.toString()
 
@@ -43,11 +48,13 @@ class LoginActivity : AppCompatActivity() {
             {
                 MainActivity.auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
                     if(it.isSuccessful){
+                        progressBar.visibility=ProgressBar.INVISIBLE
                         Toast.makeText(this, "Logged In Successfully", Toast.LENGTH_SHORT).show()
                         startActivity(Intent(this, MainActivity::class.java))
                         finish()
                     }
                 }.addOnFailureListener{
+                    progressBar.visibility=ProgressBar.INVISIBLE
                     Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
                 }
 
